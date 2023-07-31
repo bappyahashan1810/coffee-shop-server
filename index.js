266c51d6dab2e79493f2181fa3d52be8f92ef26d
bappy-ahashan <bappyahashan@gmail.com>
2023-08-01 04:01:52 +0600

    added two api for post and get all coffee

--- index.js
+++ index.js
@@ -32,12 +32,28 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const coffeeCollection = client.db('coffeeDB').collection('coffee');
+
+
+        app.get('/coffee', async (req, res) => {
+            const cursor = coffeeCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/coffee', async (req, res) => {
+            const coffee = req.body;
+            const result = await coffeeCollection.insertOne(coffee);
+            res.send(result);
+            console.log(coffee);
+
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
